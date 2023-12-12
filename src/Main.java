b820f6bed0d7025c40806d1881381566b7742caa
Saniya <saniyaanjum142115@gmail.com>
2023-12-12 09:57:56 +0530

    added the functionality of daily wages

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,24 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        double salaryPerHour = 20;
+
         int  isPresent = 0;
         int isAbsent = 1;
+        int workHours;
 
         int Attendance = (int)((Math.random()*10)%2);
 
         if(Attendance == isPresent) {
             System.out.println("Employee is present");
+            workHours = 8;
         } else {
             System.out.println("Employee is absent");
+            workHours = 0;
         }
+        double dailyWages = salaryPerHour*workHours;
+        System.out.println("daily wage of worker" + dailyWages);
+
     }
 }
