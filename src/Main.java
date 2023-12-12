0024cb28fe912a1dcf39c17e7a1cbda1d0b801a9
Saniya <saniyaanjum142115@gmail.com>
2023-12-12 16:40:53 +0530

    added 6th usecase functionality

--- src/Main.java
+++ src/Main.java
@@ -2,36 +2,45 @@ public class Main {
     public static void main(String[] args) {
 
         double salaryPerHour = 20;
-        int totalWorkingDaysInMonth = 20;
+        int totalWorkingDaysInMonth = 20; // Assuming 20 working days in a month
+        int totalWorkingHoursInMonth = 100; // Assuming 100 working hours in a month
+        int totalHoursWorked = 0;
+        int totalDaysWorked = 0;
         double monthlyWages = 0;
 
-        for (int day = 1; day <= totalWorkingDaysInMonth; day++) {
-            int Attendance = (int)((Math.random() * 10) % 3);
+        while (totalHoursWorked < totalWorkingHoursInMonth && totalDaysWorked < totalWorkingDaysInMonth) {
+            int Attendance = (int) ((Math.random() * 10) % 3);
             int workHours;
 
             switch (Attendance) {
                 case 0:
-                    System.out.println("Day " + day + ": Employee is present");
+                    System.out.println("Day " + (totalDaysWorked + 1) + ": Employee is present");
                     workHours = 8;
                     break;
                 case 1:
-                    System.out.println("Day " + day + ": Employee is absent");
+                    System.out.println("Day " + (totalDaysWorked + 1) + ": Employee is absent");
                     workHours = 0;
                     break;
                 case 2:
-                    System.out.println("Day " + day + ": Employee is present for part-time");
+                    System.out.println("Day " + (totalDaysWorked + 1) + ": Employee is present for part-time");
                     workHours = 4; // Assuming part-time hours as 4
                     break;
                 default:
-                    System.out.println("Day " + day + ": Invalid attendance status");
+                    System.out.println("Day " + (totalDaysWorked + 1) + ": Invalid attendance status");
                     workHours = 0;
                     break;
             }
+
+            // Update total hours worked and days worked
+            totalHoursWorked += workHours;
+            totalDaysWorked++;
 
             double dailyWages = salaryPerHour * workHours;
             monthlyWages += dailyWages;
         }
 
-        System.out.println("Monthly wage of worker for " + totalWorkingDaysInMonth + " days: " + monthlyWages);
+        System.out.println("Total days worked: " + totalDaysWorked);
+        System.out.println("Total hours worked: " + totalHoursWorked);
+        System.out.println("Monthly wage of worker: " + monthlyWages);
     }
 }
